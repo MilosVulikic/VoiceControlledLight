266e63e0dd95d8fd9e51b6209127e9d2245cb357
MilosVulikic <milosvulikic@gmail.com>
2018-01-16 16:48:32 +0100

    Initializing project Adding Light.cs

---
+++ VoiceControlledLight/VoiceControlledLight/Light.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Light
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Light is On");
+    }
+    public void TurnOff()
+    {
+        Console.WriteLine("Light is Off");
+    }
+}
---
+++ VoiceControlledLight/VoiceControlledLight/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceControlledLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Light diode = new Light();
+            diode.TurnOn();
+            diode.TurnOff();
+
+            Console.ReadKey();
+        }
+    }
+}
